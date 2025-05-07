759f0212b370acf175d56236764be97a56525a14
Rajesh Choudhary <rajeshchoudharyt@gmail.com>
2025-05-07 14:44:23 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	theme: {
+		content: [
+			"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+			"./components/**/*.{js,ts,jsx,tsx,mdx}",
+			"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		],
+		extend: {
+			colors: {},
+		},
+	},
+};
