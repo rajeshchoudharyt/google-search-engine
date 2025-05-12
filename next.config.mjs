620d7b16527e533e816cb1669155e91985b39abc
Rajesh Choudhary <rajeshchoudharyt@gmail.com>
2025-05-13 03:26:34 +0530

    config env

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
 			{ protocol: "https", hostname: "encrypted-tbn0.gstatic.com" },
 		],
 	},
+	env: {
+		NEXT_PUBLIC_SERPER_API_KEY: process.env.NEXT_PUBLIC_SERPER_API_KEY,
+	},
 };
 
 export default nextConfig;
